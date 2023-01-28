a440e1d9a54f1da8c6f455bd0dc92f03f98bd3a8
cirun <cirunzhang@gmail.com>
2023-01-28 00:56:10 -0500

    bug fixed

--- application.py
+++ application.py
@@ -7,11 +7,12 @@ def hello_world():
 
 @application.route('/nqueens/<n>')
 def n_queens(n):
+    n = (int)(n)
     if n > 8 or n < 1:
         return "please enter number from 1 to 8"
     head = '<h1>Solving ' + str(n) + ' Queens Problem</h1>\n\n';
     head += 'The eight queens puzzle is the problem of placing eight chess queens on an 8×8 chessboard so that no two queens threaten each other; thus, a solution requires that no two queens share the same row, column, or diagonal. There are 92 solutions. The problem was first posed in the mid-19th century. In the modern era, it is often used as an example problem for various computer programming techniques'
-    body = solve((int)(n));
+    body = solve(n);
 
     return head + body;
 
@@ -34,18 +35,18 @@ def render(arr:list):
             str += ' '
         str += '<br>'
     str += '<br>'
-    return str;    
+    return str
 
 def dfs(index:int, n:int, numQueens:int, arr:list, visitRows:set, visitCols:set, visitDiag:set, visitAnti:set, ans:list):
     if (n == numQueens):
         ans.append(render(arr))
-        return;
+        return
     
-    x = (int)(index / n);
-    y = (int)(index % n);
+    x = (int)(index / n)
+    y = (int)(index % n)
 
     if x >= n or y >= n:
-        return;
+        return
 
     if (x in visitRows or y in visitCols or x + y in visitDiag or x - y in visitAnti):
         dfs(index + 1, n, numQueens, arr, visitRows, visitCols, visitDiag, visitAnti, ans)
@@ -53,15 +54,15 @@ def dfs(index:int, n:int, numQueens:int, arr:list, visitRows:set, visitCols:set,
     dfs(index + 1, n, numQueens, arr, visitRows, visitCols, visitDiag, visitAnti, ans)
 
     arr[x][y] = 'Q'
-    visitRows.add(x);
-    visitCols.add(y);
-    visitDiag.add(x + y);
-    visitAnti.add(x - y);
+    visitRows.add(x)
+    visitCols.add(y)
+    visitDiag.add(x + y)
+    visitAnti.add(x - y)
     dfs(index + 1, n, numQueens + 1, arr, visitRows, visitCols, visitDiag, visitAnti, ans)
-    visitRows.remove(x);
-    visitCols.remove(y);
-    visitDiag.remove(x + y);
-    visitAnti.remove(x - y);
+    visitRows.remove(x)
+    visitCols.remove(y)
+    visitDiag.remove(x + y)
+    visitAnti.remove(x - y)
     arr[x][y] = '.'
     
 
